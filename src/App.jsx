91b88709bf99debac2eb5951a16644d6e253c37d
Orton Bright <ortonb110@gmail.com>
2023-03-06 09:16:30 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <main className="bg-white py-3 px-8 rounded-md w-[30rem] pb-10">
+      <p className="border-b-[1px] py-3 mb-6 text-[1.2rem] font-semibold">Dummy text</p>
+      <div className="flex flex-col items-baseline gap-3 text-[1rem] font-semibold">
+        <button className="font-bold btn">Check all</button>
+        <button className="btn">Ice-Cream</button>
+        <button className="btn">Hot-Dog</button>
+        <button className="btn">Popcorn</button>
+        <button className="btn">Cookie</button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
